ad73b1b6ed106ff904f6fede6afb5c4205e0b81c
mathias <mathias15052004@gmail.com>
2024-03-30 11:58:16 -0300

    Add variables

--- Game Console/Scenarios/Battle.cs
+++ Game Console/Scenarios/Battle.cs
@@ -1,5 +1,6 @@
 ﻿using Game_Console.Base;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,75 +12,83 @@ namespace Game_Console.Scenarios
     {
         private int firstTurn = 1;
         private int isPlayerTurn = 1;
+        private int enemyTurn;
+        private int playerAttack;
+        private int enemyAttack;
+        private int playerDefense;
+        private int enemyDefense;
 
         public Battle(bool showInfo) : base(showInfo)
         {
+            if (firstTurn == 1)
             {
-                if (firstTurn == 1 && isPlayerTurn == 1)
-                {
-                    Add("Battle starts");
-                    Add("Your turn");
-                    Add("");
-                    Add("");
-                    Add("");
-                    Add("");
-                    Add("");
-                    Add("");
-                    Add("What is going to be your move?");
-                    Add($"{Player.Instance.Name} | Life: {Player.Instance.Life}");
-                    Add("1) Attack");
-                    Add("2) Defend");
-                    Add("3) Backpack");
-                }
-                else
-                {
-                    if (isPlayerTurn == 1)
-                    {
-                        Add("Your turn");
-                        Add("");
-                        Add("");
-                        Add("");
-                        Add("");
-                        Add("");
-                        Add("");
-                        Add("What is going to be your move?");
-                        Add("1) Attack");
-                        Add("2) Defend");
-                        Add("3) Backpack");
-                    }
-                    else
-                    {
-                        Add("Enemy turn");
-                    }
-                }
+                Add("Battle starts");
+            }
 
+            if (isPlayerTurn == 1)
+            {
+                Add("Your turn");
+                Add("What is going to be your move?");
+                Add($"{Player.Instance.Name} | Life: {Player.Instance.Life}");
+                Add("1) Attack");
+                Add("2) Defend");
+                if (firstTurn == 1)
+                {
+                    firstTurn = 0;
+                }
+            }
+            else
+            {
+                Add("Enemy turn");
             }
         }
 
         public override void ProcessOption(string playerOption)
         {
-            Random rnd = new Random();
-            switch (playerOption)
+            if (Enemy.Instance.life <= 0)
             {
-                case "1":
-                    int attack = rnd.Next(1, 5);
-                    Enemy.Instance.life -= attack;
-                    Console.WriteLine($"You have dealt {attack} damage to the enemy");
-                    isPlayerTurn = 0;
-                    firstTurn = 0;
-                    Move(new Battle(false));
-                    break;
-                case "2":
-                    int defense = rnd.Next(10, 50);
-                    
-                    break;
-                case "3":
-                    break;
-                default:
-                    Console.Clear();
-                    Show();
-                    break;
+                Console.WriteLine("Enemy defeated");
             }
+
+            if (Player.Instance.Life <= 0)
+            {
+                Console.WriteLine("Game over");
+                Environment.Exit(0);
+            }
+
+            Random rnd = new Random();
+
+            if (isPlayerTurn == 1)
+            {
+                if (playerOption == "1")
+                {
+                    playerAttack = rnd.Next(1, 5);  
+                }
+                else if (playerOption == "2")
+                {
+                    playerDefense = rnd.Next(10, 50);
+                }
+                isPlayerTurn = 0;
+            }
+            else
+            {
+                enemyTurn = rnd.Next(0, 1);
+                if (enemyTurn == 0)
+                {
+                    enemyAttack = rnd.Next(2, 4);
+                    Player.Instance.Life -= enemyAttack * (playerDefense / 100);
+                    Console.WriteLine($"The enemy has dealt {enemyAttack} damage to you");
+                }
+                else
+                {
+                    enemyDefense = rnd.Next(10, 30);
+                    Player.Instance.Life -= playerAttack * (enemyDefense / 100);
+                    Console.WriteLine($"You have dealt {playerAttack} damage to the enemy");
+                }
+                isPlayerTurn = 1;
+            }
+
+            Move(new Battle(false));
         }
     }
 }
